a49d6f4486f6d9924e687d8fdf4f7e17124b83f0
nekoniumoretai <152590285+nekoniumoretai@users.noreply.github.com>
2024-03-05 08:03:55 +0000

    bootstrap,devise導入（User ,Adminのコントローラなど未作成）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "7d8b0fc6a9fc41ae8d8c6b4d94d803db.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
