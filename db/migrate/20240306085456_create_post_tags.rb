63f674a969c26763ecb43f580500137771d0b0df
nekoniumoretai <ta.chi.sho.ko@gmail.com>
2024-03-24 12:24:02 +0000

    [Fix] migration

--- db/migrate/20240306085456_create_post_tags.rb
+++ db/migrate/20240306085456_create_post_tags.rb
@@ -1,8 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration[6.1]
   def change
     create_table :post_tags do |t|
-      t.references :post, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+      t.integer :post_id, null: false
+      t.integer :tag_id, null: false
 
       t.timestamps
     end
